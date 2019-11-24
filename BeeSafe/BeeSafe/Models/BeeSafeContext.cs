fd232748b9b2c2a4222bfee297d4e4de2070cc01
cdiniz@eusou.space <cdiniz@eusou.space>
2019-11-24 06:47:20 -0300

    aaaaaaa

--- BeeSafe/BeeSafe/Models/BeeSafeContext.cs
+++ BeeSafe/BeeSafe/Models/BeeSafeContext.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BeeSafe.Model
+namespace BeeSafe.Models
 {
     public class BeeSafeContext : DbContext
     {
--- BeeSafe/BeeSafe/Startup.cs
+++ BeeSafe/BeeSafe/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BeeSafe.Models;
@@ -28,15 +29,6 @@ namespace BeeSafe
             services.AddControllers();
             services.AddScoped<Perfil>();
             string conncetionstring = @"Data Source=DESKTOP-J2RA9IJ;Initial Catalog=BeeSafe;Integrated Security=True;Pooling=False";
-            /*
- * Session - Configuração
- */
-            services.AddMemoryCache(); //Guardar os dados na memória
-            services.AddSession(options => {
-
-            });
-
-            services.AddDbContext<BeeSafeContext>(option => option.UseSqlServer(conncetionstring));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
